f50775f3c5c0c7b32be2140871550e707d4efb31
nrubera <nrubera@live.com>
2021-03-11 17:56:37 +0000

    Updated script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -3,4 +3,10 @@
 # creates a new ssh user using $1 parameter
 # adds a public key form the local repo or curled from the remote repo
 # removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+
+useradd -m -d /home/$1 -s /bin/bash $1
+mkdir /home/$1/.ssh
+cp SYS-265-03-Tech-Journal/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
+chmod 700 /home/$1/.ssh
+chmod 600 /home/$1/.ssh/authorized_keys
+chown -R $1:$1 /home/$1/.ssh
