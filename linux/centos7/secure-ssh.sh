df3acecd2b7f9a999e6f59e5bbf7aaa643634b24
nrubera <70775749+nrubera@users.noreply.github.com>
2021-03-11 11:46:22 -0500

    Delete secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++
@@ -1,6 +0,0 @@
-# secure-ssh.sh
-# author nrubera
-# creates a new ssh user using $1 parameter
-# adds a public key form the local repo or curled from the remote repo
-# removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
